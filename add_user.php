192e19ae23958eeae6f19362dbc09e0b76571875
miakojason <miakojason@gmail.com>
2023-11-13 16:33:21 +0800

    新增會員

---
+++ add_user.php
@@ -0,0 +1,24 @@
+<?php
+$dsn="mysql:host=localhost;charset=utf8;dbname=member";
+$pdo=new PDO($dsn,'root','');
+//  過濾特殊符號 https://www.php.net/manual/zh/function.trim.php
+$acc=htmlspecialchars(trim($_POST['acc']));
+
+// 注意$_POST變數，用大括號{}隔開''
+$sql="insert into `users`(`acc`,`pw`,`name`,`email`,`address`)
+                  values('{$_POST['acc']}',
+                         '{$_POST['pw']}',
+                         '{$_POST['acc']}',
+                         '{$_POST['email']}',
+                         '{$_POST['address']}')";
+ // exec檢查
+$pdo->exec($sql);
+// 不輸入資料空白註冊也會新增一筆
+// 註冊後，用header導向首頁，才不會停留在空白畫面
+header("Location:index.php");
+// 或著是導向註冊畫面
+// header("Location:reg.php");
+?>
+
+
+
---
+++ login_form.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>會員登入</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="css/style.css">
+	<style>
+        input[type='text'],
+        input[type='password']{
+            outline: none;
+        }
+    </style>
+	</head>
+	<body class="img js-fullheight" style="background-image: url(images/bg.jpg);">
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-6 text-center mb-5">
+					<h2 class="heading-section">海賊登入系統</h2>
+				</div>
+				</div>
+			<div class="row justify-content-center">
+				<div class="col-md-6 col-lg-4">
+					<div class="login-wrap p-0">
+		      	<h3 class="mb-4 text-center">你是海賊朝八嗎?</h3>
+				  <?php
+                if(isset($_GET['error'])){
+                    echo "<span style='color:red'>";
+                    echo $_GET['error'];
+                    echo "</span>";
+                }
+
+            ?>
+		      	<form action="login.php" class="signin-form" method="post">
+		      		<div class="form-group">
+		      			<input id="acc" type="text" name="acc" class="form-control" placeholder="哥蒙~哥萌~弄" required>
+		      		</div>
+	            <div class="form-group">
+	              <input id="pw" type="text" name="pw" class="form-control" placeholder="狗眼米霍克" required>
+	              <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
+	            </div>
+	            <div class="form-group">
+	            	<button type="submit" class="form-control btn btn-primary submit px-1 my-1">送出</button>
+					<button type="reset" class="form-control btn btn-primary submit px-1 my-1">重置</button>
+	            </div>
+	            <div class="form-group d-md-flex">
+	            	<div class="w-50">
+		            	<label class="checkbox-wrap checkbox-primary">記住這個朝八<!-- Remember Me -->
+									  <input type="checkbox" checked>
+									  <span class="checkmark"></span>
+									</label>
+								</div>
+								<div class="w-50 text-md-right">
+									<a href="#" style="color: #fff">忘了哪個村的朝八<!--Forgot Password--></a>
+								</div>
+	            </div>
+	          </form>
+	          <p class="w-100 text-center">&mdash; 你白吊打，井底之瓜~瓜瓜瓜 &mdash;</p>
+	          <div class="social d-flex text-center">
+	          	<a href="#" class="px-2 py-2 mr-md-1 rounded"><span class="ion-logo-facebook mr-2"></span> 非死不可</a>
+	          	<a href="#" class="px-2 py-2 ml-md-1 rounded"><span class="ion-logo-twitter mr-2"></span>踢登你</a>
+	          </div>
+		      </div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<script src="js/jquery.min.js"></script>
+  <script src="js/popper.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/main.js"></script>
+
+	</body>
+</html>
+
